05eccc36ff7e38d859548796982cbb1ab87bd017
1Ricardo5 <114586483+1Ricardo5@users.noreply.github.com>
2022-11-07 09:12:41 -0500

    part 5

--- Assets/scripts/RubyController.cs
+++ Assets/scripts/RubyController.cs
@@ -12,19 +12,22 @@ public class RubyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        rigidbody2d = GetComponent<Rididbody2D>();
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-     float horizontal = Input.GetAxis("Horizontal");
-       float vertical = Input.GetAxis("Vertical");
+      horizontal = Input.GetAxis("Horizontal");
+      vertical = Input.GetAxis("Vertical");
+     }
        
+       void FixedUpdate()
+       {
        Vector2 position = rigidbody2d.position;
        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
        position.y = position.y + 3.0f * vertical * Time.deltaTime;
-       rigidbody2d.Moveposition = position;
+       rigidbody2d.MovePosition(position);
       
     }
 }
