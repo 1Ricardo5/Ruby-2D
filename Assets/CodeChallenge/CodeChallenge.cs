6b24e2a9f29535136ec74a477a16ee4bfab1053a
1Ricardo5 <114586483+1Ricardo5@users.noreply.github.com>
2022-11-16 09:04:47 -0500

    part 7

--- Assets/CodeChallenge/CodeChallenge.cs
+++ Assets/CodeChallenge/CodeChallenge.cs
@@ -10,7 +10,7 @@ public class CodeChallenge : MonoBehaviour
     private float vertical; 
 
     // Create a variable to store the speed of the character
-    private float speed=5; 
+    private float speed = 5; 
     
     // Start is called before the first frame update
     void Start()
