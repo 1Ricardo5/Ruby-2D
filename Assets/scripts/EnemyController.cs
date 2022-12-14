a688554d0c8c1aafb329bee226cdfa503cc4c769
1Ricardo5 <114586483+1Ricardo5@users.noreply.github.com>
2022-12-14 13:23:08 -0500

    part 9

--- Assets/scripts/EnemyController.cs
+++ Assets/scripts/EnemyController.cs
@@ -11,12 +11,17 @@ public class EnemyController : MonoBehaviour
     Rigidbody2D rigidbody2D;
     float timer;
     int direction = 1;
+   
+     Animator animator
+    
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
         timer = changeTime;
+        animator = GetComponent<Animator>();
     }
+
 
     void Update()
     {
@@ -38,11 +43,15 @@ public class EnemyController : MonoBehaviour
         
         if (vertical)
         {
-            position.y = position.y + Time.deltaTime * speed * direction;
+          position.y = position.y + Time.deltaTime * speed * direction;
+          animator.SetFloat("Move X", 0);
+            animator.SetFloat("Move Y", direction);
         }
         else
         {
           position.x = position.x + Time.deltaTime * speed * direction;
+          animator.SetFloat("Move X", direction);
+            animator.SetFloat("Move Y", 0);
         }
     
         rigidbody2D.MovePosition(position);
@@ -58,3 +67,6 @@ public class EnemyController : MonoBehaviour
         }
     }
 }
+
+
+
